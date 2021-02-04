49afd234ead425f625afdf63fa859483f1bde4f9
Nurelif2708 <ehk2708@gmail.com>
2021-02-04 10:29:04 +0000

    Initial commit

---
+++ src/test/java/com/vytrack/step_definitions/Hooks.java
@@ -0,0 +1,19 @@
+package com.vytrack.step_definitions;
+
+import com.vytrack.utilities.Driver;
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+
+public class Hooks {
+    @Before
+    public void setUp(){
+        System.out.println("\tthis is coming from before ");
+    }
+
+    @After
+    public void tearDown(){
+
+        Driver.closeDriver();
+    }
+
+}
